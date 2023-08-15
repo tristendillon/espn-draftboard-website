5d61cf6a4363c51fe389d346d4ffadacf4395043
KickedSoda <96560330+KickedSoda@users.noreply.github.com>
2023-08-14 19:14:26 -0500

    auto setting?

--- backend/auth_token/management/commands/set_unix_token.py
+++ backend/auth_token/management/commands/set_unix_token.py
@@ -14,6 +14,16 @@ class Command(BaseCommand):
         os.environ["BEARER_TOKEN"] = token_str
         
         if os.environ.get('BEARER_TOKEN'):
+            shell = os.path.basename(os.environ['SHELL'])
+            
+            profile_command = f'echo "export BEARER_TOKEN={token_str}" >> ~/.{shell}rc'
+            
+            os.system(profile_command)
+            
+            self.stdout.write(self.style.SUCCESS(f'Environment variable set in {shell}rc'))
+
+            self.stdout.write(self.style.SUCCESS(f'Please restart your shell to apply changes'))
+            
             return self.stdout.write(self.style.SUCCESS(f'New token set successfully token: {token_str}'))
 
         self.stderr.write(self.style.ERROR(f'Error setting token: {token_str}\nin the system environment!'))
